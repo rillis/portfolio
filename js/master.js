dbd61b9d4182fa0ad247c249831a91c55bbad130
Rillis Vernucio <rillisnelson@gmail.com>
2022-10-21 17:16:43 -0300

    portfolio dynamic update

--- js/master.js
+++ js/master.js
@@ -7,3 +7,33 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+
+var colors = {};
+$.getJSON('https://raw.githubusercontent.com/ozh/github-colors/master/colors.json', function(data) {
+    colors = data;
+});
+
+
+function addPage(item) {
+    var portfolio = document.getElementById('portfolio');
+
+    var desc = item["description"];
+    if(desc==null){
+      desc="";
+    }
+
+
+
+    var languageStr = "<span style='margin-left:10px;font-size:21px;color:"+colors[item["language"]]["color"]+"'>⬤</span><span class='badge badge-pill badge-light' style='margin-left:5px;'>"+item["language"]+"</span>";
+
+    portfolio.innerHTML += "<div class='d-flex flex-column port-item'><div class='d-flex align-items-center port-title'>"+item["name"]+" "+languageStr+"</div><span class='port-description'>"+desc+"</span><a href='https://github.com/rillis/"+item["name"]+"' target='_blank'>Source</a></div><hr>"
+
+}
+
+$.getJSON('https://api.github.com/users/rillis/repos', function(data) {
+    data.sort(function (a, b) {
+        return a["id"]<b["id"];
+    });
+    data.forEach(addPage);
+});
